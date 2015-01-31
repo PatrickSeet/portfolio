3877108d54e6d861208300425b2cc7ed14472172
PatrickSeet <playlife00@yahoo.com>
2015-01-31 15:23:57 -0800

    settings.py update

--- patportfolio/settings.py
+++ patportfolio/settings.py
@@ -91,10 +91,11 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT,'../patportfolio')
 STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ()
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/bohor_static-files/
